13d0858d73e335bcbafdd2cbf699f26fb75b4322
Maheshshinde1191 <maheshshinde1191@gmail.com>
2023-05-29 21:55:18 +0530

    command

--- Problem_stmt_1/AddServerImpl.java
+++ Problem_stmt_1/AddServerImpl.java
@@ -2,13 +2,13 @@ import java.rmi.*;
 import java.rmi.server.*;
 
 // implements the remote interface
-public class AddServerImpl extends UnicastRemoteObject
+public class AddServerImpl extends UnicastRemoteObject implements AddServerIntf {
 
-implements AddServerIntf {
+	public AddServerImpl() throws RemoteException { 
 
-public AddServerImpl() throws RemoteException { 
-}
-public double add(double d1, double d2) throws RemoteException {
-	return d1+d2;
-}
+	}
+	
+	public double add(double d1, double d2) throws RemoteException {
+		return d1+d2;
+	}
 }
